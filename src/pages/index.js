1b2c11142f797f0afd9aa6c6b593682fce88508b
Prarthana S <prarthana-s@users.noreply.github.com>
2018-08-27 14:01:30 -0700

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to my new Gatsby site.</p>
+    <p>I am Prarthana Sannamani</p>
+    <p>I am a Computer Science student at USC.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
